0b5075ca1caac0366ac833e07413e070508577e5
Apikritch Rattanapisankul <apikritch.r@gmail.com>
2023-08-15 16:38:09 +1000

    fixed next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  basePath: "/project/codebeis",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
